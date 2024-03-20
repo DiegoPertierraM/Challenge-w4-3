f4c56b9d2e201296aa703697e41144f65ae79c8c
DiegoPertierraM <diegopmenendez@gmail.com>
2024-03-20 20:40:31 +0100

    Add rendered character cards

---
+++ projects/got/src/app/shared/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'isdi-header',
+  standalone: true,
+  imports: [],
+  template: `
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+  `,
+  styles: `
+    :host {
+      background-color: rgb(100, 60, 35);
+      height: 100px;
+      color: black;
+      text-align: center;
+      font-family: "Cinzel", serif;
+    }
+
+    h1 {
+      font-weight: 900;
+      color: #331c0e;
+    }
+
+    h2 {
+      text-align: center;
+    }
+  `,
+})
+export class HeaderComponent {
+  @Input({ required: true }) title = '';
+}
